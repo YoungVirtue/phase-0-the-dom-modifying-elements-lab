1b65576ff70ce7a43aa4e7f0985cbaa60a410752
Curtis Moses <mixedbymoses@gmail.com>
2023-06-14 10:08:55 -0700

    all passed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 main.remove();
 
 const newHeader = document.createElement("h1");
-document.body.append(newHeader);
 newHeader.id = "victory";
 newHeader.textContent = "virtue is the champion";
+document.body.append(newHeader);
